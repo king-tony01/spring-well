66fbb4149e4f87a4b6d446ac39d5283f6a882834
King Tony <128842118+king-tony01@users.noreply.github.com>
2023-11-23 19:40:51 +0100

    Update server.js
    I changed the response status code because it was responding with 200 instead of 501 when there is a server error

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ const server = http.createServer(async (req, res) => {
           res.end(JSON.stringify(await createUser(user)));
         } catch (err) {
           console.log(err);
-          res.writeHead(200, { "Content-Type": "application/json" });
+          res.writeHead(501, { "Content-Type": "application/json" });
           res.end(JSON.stringify(err));
         }
       });
